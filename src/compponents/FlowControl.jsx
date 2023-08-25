dec66ef6c88cde275eb96131ed6b9ad9dc73df19
officialeric <officialeric994@gmail.com>
2023-08-25 18:39:08 +0300

    retrieving data from databse using axios succeed

--- src/compponents/FlowControl.jsx
+++ src/compponents/FlowControl.jsx
@@ -4,52 +4,44 @@ import { useState ,useEffect} from 'react'
 import axios from 'axios'
 
 const FlowControl = () => {
-  const [flow, setflow] = useState()
-  const [flows,setdflows] = useState([])
+const [flow, setflow] = useState()
+const [flows,setflows] = useState([])
 
-  const SendData = (e) =>{
-    // e.preventDefault();
-    
-    let formData = new FormData();
-    formData.append('flow',flow);
+const SendData = (e) =>{
+// e.preventDefault();
 
-    axios({
-      method : 'post',
-      url : 'http://localhost/project-diary/index.php/',
-      data : formData ,
-      config : { header : {
-        'Content-Type' : 'multipart/form-data'
-      }}
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+let formData = new FormData();
+formData.append('flow',flow);
 
-  }
+axios({
+method : 'post',
+url : 'http://localhost/project-diary/index.php/',
+data : formData ,
+config : { header : {
+'Content-Type' : 'multipart/form-data'
+}}
+})
+.then(function (response) {
+console.log(response);
+})
+.catch(function (error) {
+console.log(error);
+});
 
-  useEffect(()=> {
-    axios
-  .get('http://localhost/project-diary/index.php/')
-  .then(function (response) {
-    console.log(response);
-  })
-  .then((data) =>{
-    setdflows({ flows : data })
-    console.log(flows);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  }, [])
-  
+}
 
-  return (
-    <div>
-      <form className='flow-form' onSubmit={SendData}>
-        <textarea name="" id="" value={flow} onChange={(e)=> setflow(e.target.value)}
+useEffect(()=> {
+axios
+.get('http://localhost/project-diary/index.php')
+.then(res => setflows(res.data))
+.catch(err => console.log(err))
+}, [])
+
+
+return (
+<div>
+  <form className='flow-form' onSubmit={SendData}>
+    <textarea name="" id="" value={flow} onChange={(e)=> setflow(e.target.value)}
               placeholder='Start flowing ...' autoFocus/>
         <button type='submit'>Create Flow</button>
       </form>
@@ -58,27 +50,34 @@ const FlowControl = () => {
         <h3>Flows</h3>
       </div> 
       <div className="flow-list">
-       
+
+       {flows.map((single)=>{
+           return (
+            <>
             <div className="flow" >
-          <div className="flow-content">
-          <p>hhh</p>
-          </div>
-          <div className="flow-footer">
-            <div className="date">
-              <p>01-01-2024 19:23 AM</p>
+            <div className="flow-content">
+            <p>
+              {single.flow}
+            </p>
             </div>
-            <div className="icons-box">
-              <div className="edit-box">
-              <AiOutlineEdit/>
+            <div className="flow-footer">
+              <div className="date">
+                <p>{single.time}</p>
               </div>
-              <div className="erase-box">
-                <AiFillDelete/>
+              <div className="icons-box">
+                <div className="edit-box">
+                <AiOutlineEdit/>
+                </div>
+                <div className="erase-box">
+                  <AiFillDelete/>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        
-        
+         </div>
+            </>
+           )
+       })}
+               
       </div>
     </div>
   )
